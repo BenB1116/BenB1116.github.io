0dde37e000ce3e94077038a6a4cf087af937ff65
BenB1116 <bburns171@gmail.com>
2023-01-08 19:48:25 -0600

    Moved Files

--- portfolio_website/scripts/greeting.js
+++
@@ -1,8 +0,0 @@
-
-var greets = ["how's it going?", "what's new?", "how do?", "what's cooking?"
-    ,"howzit?", "aloha", "holla", "what's poppin'?", "what's the haps?"];
-
-var ran = Math.floor(greets.length * Math.random());
-
-document.write(`<h1>${greets[ran].toUpperCase()}</h1>`);
-
