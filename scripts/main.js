5a4315e365f9d8824e72b2e7da32a2cf19050781
BenB1116 <bburns171@gmail.com>
2023-01-12 14:41:15 -0600

    Styled Read More button

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,20 @@
 function readMore(project) {
     let dots = document.querySelector(`.project-container[project-name="${project}"] .dots`);
     let moreText = document.querySelector(`.project-container[project-name="${project}"] .more`); 
-    let btnText = document.querySelector(`.project-container[project-name="${project}"] .myBtn`);
+    let moreImg = document.querySelector(`.project-container[project-name="${project}"] .more-img`); 
+    let btnText = document.querySelector(`.project-container[project-name="${project}"] .read-btn`);
 
+    // If dots is shown hide the more section
     if (dots.style.display === "none") {
         dots.style.display = "inline";
         btnText.textContent = "Read More";
         moreText.style.display = "none";
+        moreImg.style.display = "none";
     } else {
+        // If dots are hidden show the more section
         dots.style.display = "none";
         btnText.textContent = "Read Less"; 
         moreText.style.display = "inline";
+        moreImg.style.display = "inline";
     }
 }
